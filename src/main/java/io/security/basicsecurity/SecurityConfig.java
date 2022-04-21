9c78fef12f97e51b61aa3c35b50413dcc9cb0791
syc <syc@partdb.com>
2022-04-21 12:04:16 +0900

    Add: 사용자 정의 보안 기능 구현 @tabutabu98

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,30 @@
+package io.security.basicsecurity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        /**
+         * 인가 정책
+         */
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+        /**
+         * 인증 정책
+         */
+        http
+                .formLogin();
+    }
+}
